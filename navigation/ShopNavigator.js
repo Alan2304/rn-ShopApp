57a7d5121a5fc8535eedd7c76b983dcb77326935
Alan2304 <sanchezalan95@gmail.com>
2019-11-10 16:54:42 -0600

    Add User Product screen to the drawer navigation

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -8,6 +8,7 @@ import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen/Produ
 import ProductDetailScreen from '../screens/shop/ProductDetailScreen/ProductDetailScreen';
 import CartScreen from '../screens/shop/CartScreen/CartScreen';
 import OrderScreen from '../screens/shop/OrderScreen/OrderScreen';
+import UserProductsScreen from '../screens/user/UserProductsScreen/UserProductsScreen';
 
 import Colors from '../constants/Colors';
 import { Ionicons } from '@expo/vector-icons';
@@ -55,9 +56,24 @@ const OrdersNavigator = createStackNavigator({
     defaultNavigationOptions: defaultNavOptions
 });
 
+const AdminNavigator = createStackNavigator({
+    UserProducts: UserProductsScreen
+}, {
+    navigationOptions: {
+        drawerIcon: drawerConfig => (
+            <Ionicons 
+                name={ Platform.OS === 'android' ? 'md-create': 'ios-create' } 
+                size={23}
+                color={drawerConfig.tintColor} />
+        ) 
+    },
+    defaultNavigationOptions: defaultNavOptions
+});
+
 const ShopNavigator = createDrawerNavigator({
     Products: ProductsNavigator,
-    Orders: OrdersNavigator
+    Orders: OrdersNavigator,
+    Admin: AdminNavigator
 }, {
     contentOptions: {
         activeTintColor: Colors.primary
