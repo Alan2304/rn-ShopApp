9af98d658142d980236a9ef7d302fc86c78cf905
Alan2304 <sanchezalan95@gmail.com>
2019-11-24 15:51:02 -0600

    Add Actions and handler for the products

--- store/reducers/product.js
+++ store/reducers/product.js
@@ -1,4 +1,6 @@
 import PRODUCTS from '../../data/dummy-data';
+import { DELETE_PRODUCT, CREATE_PRODUCT, UPDATE_PRODUCT } from '../actions/product';
+import Product from '../../models/product';
 
 const initialState = {
     availableProduct: PRODUCTS,
@@ -6,5 +8,57 @@ const initialState = {
 };
 
 export default (state = initialState, action) => {
-    return state
+    switch (action.type) {
+        case CREATE_PRODUCT:
+            const newProduct = new Product(
+                new Date().toString(), 
+                'u1', 
+                action.productData.title, 
+                action.productData.imageUrl, 
+                action.productData.description, 
+                action.productData.price
+            );
+            return {
+                ...state,
+                availableProduct: state.availableProduct.concat(newProduct),
+                userProducts: state.userProducts.concat(newProduct)
+            }
+        case UPDATE_PRODUCT:
+            const productIndex = state.userProducts.findIndex(
+                prod => prod.id === action.pid
+            );
+            const updatedProduct = new Product(
+                action.pid,
+                state.userProducts[productIndex].ownerId,
+                action.productData.title,
+                action.productData.imageUrl,
+                action.productData.description,
+                state.userProducts[productIndex].price
+            );
+            const updatedUserProducts = [...state.userProducts];
+            updatedUserProducts[productIndex] = updatedProduct;
+            const availableProductIndex = state.availableProduct.findIndex(
+                prod => prod.id === action.pid
+            );
+            const updatedAvailableProducts = [...state.availableProduct];
+            updatedAvailableProducts[availableProductIndex] = updatedProduct;
+            return {
+                ...state,
+                availableProduct: updatedAvailableProducts,
+                userProducts: updatedUserProducts
+            }
+        case DELETE_PRODUCT:
+            return {
+                ...state,
+                userProducts: state.userProducts.filter(
+                    product => product.id !== action.pid
+                ),
+                availableProduct: state.availableProduct.filter(
+                    product => product.id !== action.pid
+                )
+            }
+    
+        default:
+            return state;
+    }
 }
