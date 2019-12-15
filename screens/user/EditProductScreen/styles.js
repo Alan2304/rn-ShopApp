ed9f04b3a9545e5bcccb08d0ab99680887a4bd49
Alan2304 <sanchezalan95@gmail.com>
2019-12-15 15:50:10 -0600

    Add input component

--- screens/user/EditProductScreen/styles.js
+++ screens/user/EditProductScreen/styles.js
@@ -4,19 +4,7 @@ const styles = StyleSheet.create({
     form: {
         margin: 20
     },
-    formControl: {
-        width: '100%'
-    },
-    label: {
-        fontFamily: 'open-sans-bold',
-        marginVertical: 8
-    },
-    input: {
-        paddingHorizontal: 2,
-        paddingVertical: 5,
-        borderBottomColor: '#ccc',
-        borderBottomWidth: 1
-    }
+    
 });
 
 export default styles;
