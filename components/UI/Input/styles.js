2d270fb8b28ea729ef1019305d372825ee5c6859
Alan2304 <sanchezalan95@gmail.com>
2019-12-15 15:48:42 -0600

    Add Input standard component to handle validation and to reuse

---
+++ components/UI/Input/styles.js
@@ -0,0 +1,27 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    formControl: {
+        width: '100%'
+    },
+    label: {
+        fontFamily: 'open-sans-bold',
+        marginVertical: 8
+    },
+    input: {
+        paddingHorizontal: 2,
+        paddingVertical: 5,
+        borderBottomColor: '#ccc',
+        borderBottomWidth: 1
+    },
+    errorContainer: {
+        marginVertical: 5
+    },
+    errorText: {
+        fontFamily: 'open-sans',
+        color: 'red',
+        fontSize: 13
+    }
+});
+
+export default styles;
