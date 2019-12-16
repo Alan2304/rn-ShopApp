91b11bf4948a8f376d4ddf1d4db7ab31f511ae04
Alan2304 <sanchezalan95@gmail.com>
2019-12-15 18:10:57 -0600

    Add Redux thunk

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import { AppLoading } from 'expo'
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import * as Font from 'expo-font';
+import ReduxThunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 import productsReducer from './store/reducers/product';
@@ -17,7 +18,7 @@ const rootReducer = combineReducers({
   orders: orderReducer
 });
 
-const store = createStore(rootReducer, composeWithDevTools());
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 
 const fetchFonts = () => {
   return Font.loadAsync({
